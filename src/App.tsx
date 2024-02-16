0e22ac5a0a161852e45c91c1fc30f412001ddc80
jondujaka <jdujaka@gmail.com>
2024-02-16 16:09:31 +0100

    remove debugging code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useEffect } from "react";
 import "./App.css";
 import Carousel from "./Carousel/Carousel";
 
 function App() {
-  useEffect(() => {
-    console.log("app");
-  }, []);
   return <Carousel />;
 }
 
